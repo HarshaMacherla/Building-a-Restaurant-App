aecd0bcbb73e4c7e42bf1404e26e28a856b7fef3
HarshaMacherla <macherlaharsha997@gmail.com>
2023-05-05 16:16:50 +0530

    Built header for Restaurant App

---
+++ src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from "./HeaderCartButton.module.css";
+import CartIcon from "../Cart/CartIcon";
+
+function HeaderCartButton(props) {
+  return (
+    <button className={styles.button}>
+      <span className={styles.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={styles.badge}>0</span>
+    </button>
+  );
+}
+
+export default HeaderCartButton;
