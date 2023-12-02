3c62bb3f368db70e5f32d296f1dfd304d9f9a54c
HarshaMacherla <macherlaharsha997@gmail.com>
2023-12-02 21:25:16 +0530

    Changed UI elements and menu items

---
+++ src/components/Meals/Menu.js
@@ -0,0 +1,34 @@
+const MENU = {
+  starters: [
+    {
+      id: "s1",
+      name: "Palak Tikka",
+      description:
+        "Paneer, marinated in a spiced yogurt-based marinade, arranged on skewers and grilled in the oven",
+      price: 220,
+    },
+    {
+      id: "s2",
+      name: "Paneer Roll",
+      description:
+        "Tasty paneer stuffing paired together with healthy veggie salad and bright green chutney.",
+      price: 100,
+    },
+    {
+      id: "s3",
+      name: "Tandoori Gobi",
+      description:
+        "Marinated cauliflower florets which are later grilled in an oven or tandoor. A scrumptious appetizer recipe.",
+      price: 180,
+    },
+    {
+      id: "s4",
+      name: "Mushroom Tikka",
+      description:
+        "Tender and succulent mushrooms that are marinated and later baked or grilled.",
+      price: 200,
+    },
+  ],
+};
+
+export default MENU;
